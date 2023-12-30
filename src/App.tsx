265b592298f19f2e8ebf9d465b90a36c09a088ca
trueai-org <circle@trueai.org>
2023-12-30 11:48:28 +0800

    ico, main

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,146 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button, Space, DatePicker, version } from "antd";
 
-import {
-  PageContainer,
-  ProBreadcrumb,
-  ProLayout,
-} from "@ant-design/pro-components";
+import { ProLayout } from "@ant-design/pro-components";
+
+import defaultProps from "./_defaultProps";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [pathname, setPathname] = useState("/list/sub-page/sub-sub-page1");
 
   return (
     <ProLayout
+      title="MDrive"
+      logo={<img src="/logo.png" style={{ width: 24, height: 24 }} />}
+      {...defaultProps}
       location={{
-        pathname: "/admin/process/edit/123",
+        pathname,
       }}
-      layout="mix"
-      ErrorBoundary={false}
-      headerContentRender={() => <ProBreadcrumb />}
-      breadcrumbRender={(routers = []) => [
-        {
-          path: "/",
-          title: "主页",
-        },
-        {
-          path: "/",
-          title: "测试页",
-        },
-        ...routers,
-      ]}
-      menuDataRender={() => [
-        {
-          path: "/welcome",
-          name: "欢迎",
-        },
-        {
-          path: "/admin",
-          name: "管理",
-          children: [
-            {
-              name: "申请单列表",
-              path: "/admin/process",
-            },
-            {
-              name: "申请单详情",
-              path: "/admin/process/detail/:id",
-              hideInMenu: true,
-            },
-            {
-              name: "编辑申请单",
-              path: "/admin/process/edit/:id",
-              hideInMenu: true,
-            },
-            {
-              name: "添加申请单",
-              path: "/admin/process/add",
-              hideInMenu: true,
-            },
-          ],
-        },
-      ]}
+      onMenuHeaderClick={(e) => console.log("e", e)}
+      menuItemRender={(item, dom) => (
+        <a
+          onClick={() => {
+            setPathname(item.path || "/welcome");
+          }}
+        >
+          {dom}
+        </a>
+      )}
+      layout="top"
     >
-      {/* <Content /> */}
-      <PageContainer
-        className="w-full"
-        content="欢迎使用"
-        breadcrumbRender={false}
+      这是内容区域
+      {/* <PageContainer
+        extra={[
+          <Button key="3">操作</Button>,
+          <Button key="2">操作</Button>,
+          <Button key="1" type="primary">
+            主操作
+          </Button>,
+        ]}
+        footer={[
+          <Button key="3">重置</Button>,
+          <Button key="2" type="primary">
+            提交
+          </Button>,
+        ]}
       >
-        <div>Hello World</div>
-
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-
-        <div style={{ padding: "0 24px" }}>
-          <h1 className="text-red-400">antd version: {version}</h1>
-          <Space>
-            <DatePicker />
-            <Button type="primary">Primary Button</Button>
-          </Space>
-        </div>
-
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </PageContainer>
+        <ProCard
+          style={{
+            height: "200vh",
+            minHeight: 800,
+          }}
+        >
+          <div />
+        </ProCard>
+      </PageContainer> */}
     </ProLayout>
+
+    // <div className="m-auto max-w-[800px] h-screen shadow">
+    //   <ProCard split="vertical">
+    //     <ProCard className="h-screen" title="Logo" colSpan={6}>
+    //       左侧内容
+    //     </ProCard>
+    //     <ProCard title="内容">
+    //       <div>右侧内容</div>
+    //     </ProCard>
+    //   </ProCard>
+    // </div>
+    // <ProLayout
+    //   title="MDrive"
+    //   logo={false}
+    //   ErrorBoundary={false}
+    //   menuDataRender={() => [
+    //     {
+    //       path: "/welcome",
+    //       name: "欢迎",
+    //     },
+    //     {
+    //       path: "/admin",
+    //       name: "管理",
+    //       children: [
+    //         {
+    //           name: "申请单列表",
+    //           path: "/admin/process",
+    //         },
+    //         {
+    //           name: "申请单详情",
+    //           path: "/admin/process/detail/:id",
+    //           hideInMenu: true,
+    //         },
+    //         {
+    //           name: "编辑申请单",
+    //           path: "/admin/process/edit/:id",
+    //           hideInMenu: true,
+    //         },
+    //         {
+    //           name: "添加申请单",
+    //           path: "/admin/process/add",
+    //           hideInMenu: true,
+    //         },
+    //       ],
+    //     },
+    //   ]}
+    // >
+
+    //   <PageContainer
+    //     className="w-full"
+    //     content="欢迎使用"
+    //     breadcrumbRender={false}
+    //   >
+    //     <div>Hello World</div>
+
+    //     <div>
+    //       <a href="https://vitejs.dev" target="_blank">
+    //         <img src={viteLogo} className="logo" alt="Vite logo" />
+    //       </a>
+    //       <a href="https://react.dev" target="_blank">
+    //         <img src={reactLogo} className="logo react" alt="React logo" />
+    //       </a>
+    //     </div>
+
+    //     <div style={{ padding: "0 24px" }}>
+    //       <h1 className="text-red-400">antd version: {version}</h1>
+    //       <Space>
+    //         <DatePicker />
+    //         <Button type="primary">Primary Button</Button>
+    //       </Space>
+    //     </div>
+
+    //     <div className="card">
+    //       <button onClick={() => setCount((count) => count + 1)}>
+    //         count is {count}
+    //       </button>
+    //       <p>
+    //         Edit <code>src/App.tsx</code> and save to test HMR
+    //       </p>
+    //     </div>
+    //     <p className="read-the-docs">
+    //       Click on the Vite and React logos to learn more
+    //     </p>
+    //   </PageContainer>
+    // </ProLayout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,16 @@ import { ConfigProvider } from "antd";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConfigProvider
-    // theme={{
-    //   token: {
-    //     // Seed Token，影响范围大
-    //     colorPrimary: "#00b96b",
-    //     borderRadius: 2,
+    theme={{
+      token: {
+        // Seed Token，影响范围大
+        colorPrimary: "#00b96b",
+        borderRadius: 2,
 
-    //     // 派生变量，影响范围小
-    //     colorBgContainer: "#f6ffed",
-    //   },
-    // }}
+        // 派生变量，影响范围小
+        colorBgContainer: "#f6ffed",
+      },
+    }}
     >
       <App />
     </ConfigProvider>
